f33f4701fd955db54cc0402528275542e66a2dd0
usrcdimi <usrcdimi@MICDI132-019.cibertec.edu.pe>
2017-09-06 14:42:19 -0500

    GITclass

---
+++ FIrtsProject/src/beans/Student.java
@@ -0,0 +1,49 @@
+package beans;
+
+public class Student {
+	private int id;
+	private String name;
+	private String lastName;
+	private String direction;
+	private int CellPhone;
+	private int age;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getDirection() {
+		return direction;
+	}
+	public void setDirection(String direction) {
+		this.direction = direction;
+	}
+	public int getCellPhone() {
+		return CellPhone;
+	}
+	public void setCellPhone(int cellPhone) {
+		CellPhone = cellPhone;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+
+}
---
+++ FIrtsProject/src/beans/Worker.java
@@ -0,0 +1,50 @@
+package beans;
+
+public class Worker {
+	private int id;
+	private String name;
+	private String lastName;
+	private String address;
+	private String fecha;
+	private int edad;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getFecha() {
+		return fecha;
+	}
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	
+
+}
